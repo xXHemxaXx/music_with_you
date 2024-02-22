71d8c3ef924bcecf71c7cea72cc7db894e12be20
Valerie2107 <kinkamyl@hotmail.com>
2024-02-22 11:43:21 +0100

    securite

--- script/main.js
+++ script/main.js
@@ -4,24 +4,33 @@ let myBtns = document.querySelector(".buttons")
 
 
 // fetch(`https://deezerdevs-deezer.p.rapidapi.com/infos?rapidapi-key=19b3d652a3msh4937caeed36c230p1b3c54jsn1da2ad719459&rapidapi-host=deezerdevs-deezer.p.rapidapi.com`) fonctionne pour info en général
-//ici on teste avec ma info - ça marche
+//ici on teste avec info - ça marche
 // aussi testé avec ma playlist plaisir coupable - playlist/8248370362 - ça marche
 
 
 
-function sortMusic(attribute){  //attribute correspondra au data-attribute en argument qui sera dans le eventListener
+function sortMusic(music){  //attribute correspondra au data-attribute en argument qui sera dans le eventListener
 
-  fetch(`https://deezerdevs-deezer.p.rapidapi.com/${attribute}/8248370362?rapidapi-key=6e9b5b7007msh37e933091d37056p1da7e9jsnb8a274dd7b51&redirect_uri=deezerdevs-deezer.p.rapidapi.com`)
+  fetch(`https://deezerdevs-deezer.p.rapidapi.com/${music}?rapidapi-key=6e9b5b7007msh37e933091d37056p1da7e9jsnb8a274dd7b51&redirect_uri=deezerdevs-deezer.p.rapidapi.com`)
   .then(response => response.json())
   .then(data => {
     console.log(data);
+
+
+
+
+
   })
+  
   .catch(error => {console.log("Erreur lors de la récup des données :", error);
   })
 
 }
 
 myBtns.addEventListener('click', function(e){
+  if(e.target.classList.contains("btn")){  //pour vérifier si on est bien sur un bouton avec la classList btn
 
-  sortMusic(e.target.getAttribute("data-attribute"))
+    sortMusic(e.target.getAttribute("data-music"))
+
+  }
 })
