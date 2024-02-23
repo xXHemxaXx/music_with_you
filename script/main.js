cefae4f60356615cbd22535647f284ec5e66d36a
Laura-Lepage <lauralepage02@hotmail.com>
2024-02-23 11:25:49 +0100

    modifications css header

--- script/main.js
+++ script/main.js
@@ -33,3 +33,32 @@ myBtns.addEventListener('click', function(e){
 
   }
 })
+
+//scroll au menu
+let navbar = document.querySelector(".navbar")
+let newScrollay = 0
+
+if (window.innerWidth > 1000){
+  window.addEventListener('scroll', function(){
+      let dejaScrollay = window.scrollY
+      if (dejaScrollay > newScrollay){
+          navbar.classList.add("slide")
+      } else {
+          navbar.classList.remove("slide")
+      }
+      newScrollay = dejaScrollay   
+  })
+
+}
+
+
+//Faire apparaître/disparaitre le menu en fonction du scroll (rajouter classe sticky à la nav pour que ça fonctionne)
+
+fetch(`https://deezerdevs-deezer.p.rapidapi.com/infos?rapidapi-key=19b3d652a3msh4937caeed36c230p1b3c54jsn1da2ad719459&rapidapi-host=deezerdevs-deezer.p.rapidapi.com`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    
+  })
+  .catch(error => {console.log("Erreur lors de la récup des données :", error);
+})
