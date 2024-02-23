d8969c8be0ee98c8acb44bdff7193e04cdbbb5cd
valerie2107 <kinkamyl@hotmail.com>
2024-02-23 10:29:46 +0100

    ok dans vscode

--- script/main.js
+++ script/main.js
@@ -1,20 +1,24 @@
-let googleMusic = document.querySelector(".searchbar button")
-let googleInput = document.querySelector(".searchbar input")
-let resultatsRecherche
-let fiche
-
 let myBtns = document.querySelector(".buttons")
 
+
+
+
 // fetch(`https://deezerdevs-deezer.p.rapidapi.com/infos?rapidapi-key=19b3d652a3msh4937caeed36c230p1b3c54jsn1da2ad719459&rapidapi-host=deezerdevs-deezer.p.rapidapi.com`) fonctionne pour info en général
+//ici on teste avec info - ça marche
+// aussi testé avec ma playlist plaisir coupable - playlist/8248370362 - ça marche
 
-function searchMusic(search){  //search correspondra au googleInput.value en argument qui sera dans le googleMusic.addEventListener
 
-  fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${search}&rapidapi-key=6e9b5b7007msh37e933091d37056p1da7e9jsnb8a274dd7b51&redirect_uri=deezerdevs-deezer.p.rapidapi.com`)
+
+function sortMusic(music){  //attribute correspondra au data-attribute en argument qui sera dans le eventListener
+
+  fetch(`https://deezerdevs-deezer.p.rapidapi.com/${music}?rapidapi-key=6e9b5b7007msh37e933091d37056p1da7e9jsnb8a274dd7b51&redirect_uri=deezerdevs-deezer.p.rapidapi.com`)
   .then(response => response.json())
   .then(data => {
-    console.log(data); //en réponse on aura plusieurs résultats 
-    //je dois ranger et stocker mes resultats dans un let qui sera un array que je vais créer en dehors de cette fonction pour pouvoir l'utiliser partout (let resultatsRecherche) --> scope
-    /* resultatsRecherche = data.data*/
+    console.log(data);
+
+
+
+
 
   })
   
@@ -23,18 +27,9 @@ function searchMusic(search){  //search correspondra au googleInput.value en arg
 
 }
 
-
-googleMusic.addEventListener('click', function(event){
-  event.preventDefault(); // ma page ne sera pas rechargée par défaut bien que le clic ait lieu à l'intérieur d'un form  https://developer.mozilla.org/fr/docs/Web/API/Event/preventDefault
-  // console.log('pierre a appuyé sur son bouton');
-  searchMusic(googleInput.value)
-
-})
-
-
 myBtns.addEventListener('click', function(e){
-  if(e.target.classList.contains("btn")){  //pour vérifier si on est bien sur un bouton avec la classList btn  
-    sortMusic(e.target.getAttribute("data-music"))
+  if(e.target.classList.contains("btn")){  //pour vérifier si on est bien sur un bouton avec la classList btn
+  sortMusic(e.target.getAttribute("data-music"))
 
   }
 })
