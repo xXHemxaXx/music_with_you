35a1ea7c3d701fe9fab18eefcbcf55cb68da78f5
xXHemxaXx <mimi.hemxa@gmail.com>
2024-02-23 09:05:55 +0100

    HTML et CSS du MAIN

--- script/main.js
+++ script/main.js
@@ -3,19 +3,41 @@ let Hemza = `57f45402fdmshf4aae5ddcf4cc46p157993jsn931effea4595`
 let number = 2
 
 
-fetch(`https://deezerdevs-deezer.p.rapidapi.com/genre/${number}?rapidapi-key=${Hemza}&rapidapi-host=deezerdevs-deezer.p.rapidapi.com`)
-    .then(response => response.json())
-    .then(datas => {
-        console.log(datas);
-    })
-    .catch(error => {console.log("Erreur lors de la récup des données :", error);
-})
+// fetch(`https://deezerdevs-deezer.p.rapidapi.com/genre/${number}?rapidapi-key=${Hemza}&rapidapi-host=deezerdevs-deezer.p.rapidapi.com`)
+//     .then(response => response.json())
+//     .then(datas => {
+//         console.log(datas);
+//     })
+//     .catch(error => {console.log("Erreur lors de la récup des données :", error);
+// })
 
+// swiper-banner
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
-    spaceBetween: 30,
+    spaceBetween: 20,
+    width: 950,
     pagination: {
-        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+// swiper-album
+var swiperAlbum = new Swiper(".mySwiper-album", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    pagination: {
+        clickable: true,
+    },
+});
+
+// swiper-album-double
+var swiperDoubleAlbum = new Swiper(".mySwiper-double", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    grid: {
+        rows:2,
+    },
+    pagination: {
         clickable: true,
     },
 });
